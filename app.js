41d2f1ca84492a7f2229be0d0c57808c58b5fd63
Jose Scharneck <jose.scharneck87@gmail.com>
2024-01-18 19:07:19 +0200

    extra

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.static("public"));
+
 const PORT = process.env.PORT | 3000;
 
-app.get("/", (req, res) => {
-  res.send("hello azure");
-});
+// app.get("/", (req, res) => {
+//   res.send("hello azure");
+// });
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
