633165e87483534de2dfe68ccf08f26d2d8184d4
Jose Scharneck <jose.scharneck87@gmail.com>
2024-01-18 16:26:59 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello azure");
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 80");
+});
